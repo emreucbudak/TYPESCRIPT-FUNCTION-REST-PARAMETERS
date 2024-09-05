e2627f97ec6c4cb0d2b32298a7a3cc5a5e61f056
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-09-05 21:12:10 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function toplam() {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    console.log(numbers);
+}
+// REST PARAMETERSLAR EN SONDA TANIMLANMALI
+toplam(10, 20, 30, 40, 60);
---
+++ app.ts
@@ -0,0 +1,5 @@
+function toplam (...numbers:number[]) {
+    console.log(numbers)
+}
+// REST PARAMETERSLAR EN SONDA TANIMLANMALI
+toplam(10,20,30,40,60)
